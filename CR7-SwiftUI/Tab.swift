b2f9f451c03c9bef1dc89955a4b4d12f6a7146f6
Swantan Barua <work.swantan@gmail.com>
2026-06-02 13:09:59 +0530

    Create Tab Enum

---
+++ CR7-SwiftUI/Tab.swift
@@ -0,0 +1,30 @@
+//
+//  Tab.swift
+//  CR7-SwiftUI
+//
+//  Created by Swantan Barua on 02/06/26.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    
+    case home
+    case goals
+    case opp
+    case keep
+    case stats
+    
+    var icon: String {
+        
+        switch self {
+            
+        case .home: return "house"
+        case .goals: return "trophy"
+        case .opp: return "hand.draw.fill"
+        case .stats: return "barchart"
+        default: return "house"
+            
+        }
+    }
+}
