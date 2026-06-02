0a6de130d0b2544bf320f3da9f24547c0c5da0ff
Swantan Barua <work.swantan@gmail.com>
2026-06-02 13:57:40 +0530

    Add OppView and StatsView SwiftUI components
    Create two new SwiftUI view files: OppView.swift and StatsView.swift. Each file contains a simple placeholder View with a Text label and a preview scaffold to serve as starting points for implementing the opponent and stats screens.

---
+++ CR7-SwiftUI/OppView.swift
@@ -0,0 +1,22 @@
+//
+//  OppView.swift
+//  CR7-SwiftUI
+//
+//  Created by Swantan Barua on 02/06/26.
+//
+
+import SwiftUI
+
+struct OppView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text("Opp")
+    }
+}
+
+#Preview {
+    OppView()
+}
---
+++ CR7-SwiftUI/StatsView.swift
@@ -0,0 +1,22 @@
+//
+//  StatsView.swift
+//  CR7-SwiftUI
+//
+//  Created by Swantan Barua on 02/06/26.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text("Stats")
+    }
+}
+
+#Preview {
+    StatsView()
+}
