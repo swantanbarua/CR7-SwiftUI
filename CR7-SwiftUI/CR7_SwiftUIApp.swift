123095d5db67c05d86662fe5533394bce514ca30
Swantan Barua <work.swantan@gmail.com>
2026-06-02 13:02:25 +0530

    Initial Commit

---
+++ CR7-SwiftUI/CR7_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CR7_SwiftUIApp.swift
+//  CR7-SwiftUI
+//
+//  Created by Swantan Barua on 02/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CR7_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
